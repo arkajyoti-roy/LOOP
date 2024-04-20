927d1f280c7dc0aaa4c922b4b31f0cacf6d77b82
arka445 <163657852+arka445@users.noreply.github.com>
2024-04-20 18:39:21 +0530

    Css Fixed

--- src/Compiler.jsx
+++ src/Compiler.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
 <div className="comp">
-  <br /><br /><br />
+  <br /><br />
       <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
         <CodeEditor />
       </Box>
