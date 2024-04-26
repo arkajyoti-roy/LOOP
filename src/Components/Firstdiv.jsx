0923443ce8613d85683d207ab244473063560232
ᴄᴏᴅᴇʀ ᴀɴᴊɪ <92791382+cromaguy@users.noreply.github.com>
2024-04-26 20:27:56 +0530

    Updated Banner

--- src/Components/Firstdiv.jsx
+++ src/Components/Firstdiv.jsx
@@ -4,9 +4,15 @@ import "./Top.css";
 const Firstdiv = () => {
   return (
     <>
-      <div className="first">
-        <img src="" className="" alt="" />
-      </div>
+      {/* <div className="first">
+        <img src="https://cdn.vectorstock.com/i/preview-1x/57/43/bright-blue-tech-communication-banner-vector-46185743.jpg" className="banner" alt="Hello" />
+        <p class="banner_text">Welcome to LOOP!</p>
+      </div> */}
+      <section class="header">
+        <h1>Banner Text</h1>
+        <p>Details</p>
+        <a class="btn-bgstroke">Button</a>
+      </section>
     </>
   );
 };
